829e2af2904e412d5b8a529a8bdc1a2c76cc01d8
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-15 09:08:49 -0800

    starting render utils

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // import functions and grab DOM elements
-
+import { getWitches } from './fetch-utils.js';
+import { renderWitches } from './render-utils.js';
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+getWitches();
---
+++ render-utils.js
@@ -0,0 +1,9 @@
+export function renderWitches(witches) {
+    const witchDiv = document.createElement('div');
+    const witchType = document.createElement('p');
+    const witchPowerLevel = document.createElement('p');
+    const witchPowerType = document.createElement('p');
+    const witchUses = document.createElement('p');
+    
+
+}
