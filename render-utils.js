39e23d624fd7fe6d071b9642d5c342ee30801993
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-10 21:14:08 -0800

    added assets, css on images

--- render-utils.js
+++ render-utils.js
@@ -2,20 +2,16 @@ export function renderWitches(witch) {
     const witchDiv = document.createElement('div');
     const a = document.createElement('a');
     const witchType = document.createElement('p');
-    const witchPowerLevel = document.createElement('p');
-    const witchPowerType = document.createElement('p');
     const witchImg = document.createElement('img');
 
     witchDiv.classList.add('witch-hold');
 
     witchType.textContent = witch.type;
-    witchPowerLevel.textContent = `Power level: ${witch.power_level}`;
-    witchPowerType.textContent = `Power type: ${witch.power_type}`;
 
     witchImg.src = `./assets/witch-${witch.type}-trans.png`;
     a.href = `./details/?id=${witch.id}`;
 
-    witchDiv.append(witchType, witchPowerLevel, witchPowerType, witchImg);
+    witchDiv.append(witchImg, witchType);
 
     a.append(witchDiv);
 
